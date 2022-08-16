3bb2027d51bfd38fff58c740a8d2d5c714617987
Olufemi Okikioluwa <51975725+Kikks@users.noreply.github.com>
2022-08-16 23:29:15 +0100

    🛑 Error handling

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -89,4 +89,29 @@ def create_app(test_config=None):
         except:
             abort(422)
 
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({"success": False, "error": 404, "message": "Not found."}), 404
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return (
+            jsonify({"success": False, "error": 500, "message": "Server error."}),
+            500,
+        )
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return (
+            jsonify({"success": False, "error": 422, "message": "Unprocessable."}),
+            422,
+        )
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        return (
+            jsonify({"success": False, "error": 405, "message": "Method not allowed."}),
+            405,
+        )
+
     return app
